eadd5316b7c2da85c7b00a9f456f1f4cc5c1384f
Maria <malett@ttu.ee>
2018-02-27 14:49:08 +0200

    initial project

---
+++ wakepark/src/main/java/idk0071/ttu/WakeparkApplication.java
@@ -0,0 +1,12 @@
+package idk0071.ttu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakeparkApplication.class, args);
+	}
+}
---
+++ wakepark/src/test/java/idk0071/ttu/WakeparkApplicationTests.java
@@ -0,0 +1,16 @@
+package idk0071.ttu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WakeparkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
