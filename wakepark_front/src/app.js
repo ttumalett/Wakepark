7978683428bca0f3e64a5d2ac216c30c020383cd
Lisa <lisa.nyyd@gmail.com>
2018-04-04 12:32:45 +0300

    Database communication

--- wakepark_front/src/app.js
+++ wakepark_front/src/app.js
@@ -9,7 +9,8 @@ export class App {
       {route: ['', 'home'], name: 'home', moduleId: 'home/index', title: "Avaleht", nav: true},
       {route: 'login', name: 'login', moduleId: 'login/login', title: "Logi sisse", nav: true},
       {route: 'register', name: 'register', moduleId: 'register/register', title: 'Registreeri', nav: true},
-      {route: 'contacts', name: 'contacts', moduleId: 'contacts/contacts', title: 'Kontaktid', nav: true}
+      {route: 'contacts', name: 'contacts', moduleId: 'contacts/contacts', title: 'Kontaktid', nav: true},
+      {route: 'info', name: 'info', moduleId: 'info/info', title: 'Info', nav: true}
       ]
     )
   }
--- wakepark_front/src/login/login.js
+++ wakepark_front/src/login/login.js
@@ -1,8 +1,21 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class login {
   constructor() {
   }
 
-  register() {
-    console.log("jah");
+  data = {};
+
+  login() {
+    let client = new HttpClient();
+    client.fetch('http://localhost:8080', {
+      'method': "POST",
+      'body': json(this.data)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Server saatis " + data.username + " " + data.password)
+      });
+
   }
 }
